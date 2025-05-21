dfbafbc85b4e982709fd835df3c62b8c2352a2e1
wsm.tycoon <wsm.tycoon@yandex.ru>
2025-05-21 23:14:09 +0400

    JsonWsm: Add MIT LICENSE.

--- Source/JsonWsm.cpp
+++ Source/JsonWsm.cpp
@@ -1,3 +1,15 @@
+/**************************************************************************/
+/**                  Created by WsmTycoon on 29.04.2025                  **/
+/**************************************************************************/
+
+/** You are solely responsible for determining the appropriateness of using
+ * or redistributing the Work and assume any risks associated with Your
+ * exercise of permissions under this License. **/
+/** Software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. **/
+	
 #include "JsonWsm.h"
 #include <regex>
 #include <sstream>
